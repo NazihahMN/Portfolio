8a909243334373d4e0464eb006ed5ac2fd7c1e90
NazihahMN <nazihahmohdnazrii@gmail.com>
2022-05-08 22:53:09 +0800

    color changed

--- nazihahportfolio/src/App.js
+++ nazihahportfolio/src/App.js
@@ -35,7 +35,7 @@ function App() {
     <section className="home" id="home">
         <div className="max-width">
             <div className="home-content">
-                <div className="text-1">Hello, My Name is</div>
+                <div className="text-1">Hello, I am</div>
                 <div className="text-2">Nazihah Nazri</div>
             </div>
         </div>
